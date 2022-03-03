45c7b71c37d3c8e88bb7e03316e24568bae7b5fd
the-chat <thechatllc@gmail.com>
2022-03-03 17:47:38 +0500

    getFullname

---
+++ src/utils/getFullname.ts
@@ -0,0 +1,18 @@
+import {TFunction} from "next-i18next"
+import {BaseUserData} from "@the-chat/types"
+
+const getFullname = (
+  {displayName, email, phoneNumber}: BaseUserData,
+  voc: TFunction
+) => {
+  const emailStr = email ? ` <${email}>` : ""
+  const phoneStr = phoneNumber ? ` [${phoneNumber}]` : ""
+
+  const fullname = displayName + emailStr + phoneStr
+  const withoutDisplayName = emailStr.trim() + phoneStr
+
+  // old comment: name can not exist, if you signed in with google
+  return displayName ? fullname : withoutDisplayName || voc("haveNotAccount")
+}
+
+export default getFullname
