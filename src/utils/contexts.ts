9e85cb28b1e50191a8e7efb63f54638ea209bf73
the-chat <thechatllc@gmail.com>
2022-03-03 17:49:42 +0500

    Config

---
+++ src/utils/contexts.ts
@@ -0,0 +1,19 @@
+import genContext, {
+  genStateContext,
+  SimpleProviderWrapper,
+} from "@the-chat/gen-context"
+import {BaseUserData, State} from "@the-chat/types"
+import getUser, {AllUserData} from "@the-chat/use-user"
+
+const [useConfig, ConfigProvider] = genContext<{
+  sidebarOpen: State<boolean>
+  // todo: use-user: type default value
+  user: AllUserData<BaseUserData>
+}>(SimpleProviderWrapper, {
+  sidebarOpen: [false, () => {}],
+  user: null,
+})
+
+export const useSidebarOpen = () => useConfig().sidebarOpen
+export const useUser = () => useConfig().user
+export {ConfigProvider}
