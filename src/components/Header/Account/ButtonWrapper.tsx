75827bcf1abf635e91afc4e18dda66e6fe5dd91b
the-chat <thechatllc@gmail.com>
2022-03-07 18:31:31 +0500

    Header ButtonWrapper

---
+++ src/components/Header/Account/ButtonWrapper.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from "react"
+import { IconButton, IconButtonProps, Theme } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+import TooltipWithOnClick from "../../TooltipWithOnClick"
+import headerButtonStyles from "../../../utils/headerButtonStyles"
+
+const useStyles = makeStyles<Theme>((theme) => ({
+  open: headerButtonStyles(theme, true),
+}))
+
+const ButtonWrapper: FC<
+  Pick<IconButtonProps, "onClick"> & {
+    title: string
+  }
+> = ({ children, onClick, title }) => {
+  const { open } = useStyles()
+
+  return (
+    <TooltipWithOnClick
+      title={title}
+      tooltipProps={{
+        className: open,
+      }}
+      ComponentTag={IconButton}
+      componentProps={{
+        style: {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "centet",
+        },
+        ...(onClick && { onClick }),
+      }}
+    >
+      {children}
+    </TooltipWithOnClick>
+  )
+}
+
+export default ButtonWrapper
