a7ad09967b003f79a657f3068025e5c243f58f92
the-chat <thechatllc@gmail.com>
2022-03-07 18:31:17 +0500

    Header NewAccount

---
+++ src/components/Header/Account/NewAccount.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Avatar } from "@mui/material"
+import Link from "next/link"
+import ButtonWrapper from "./ButtonWrapper"
+import { AddReaction } from "@mui/icons-material"
+import { useTranslation } from "next-i18next"
+import { useGetSSOLink } from "../../../utils/contexts"
+
+const NewAccount = () => {
+  const { t } = useTranslation("ui-kit-header")
+
+  return (
+    <Link href={useGetSSOLink()}>
+      <ButtonWrapper title={t("new-acc-tooltip-title")}>
+        <Avatar>
+          <AddReaction />
+        </Avatar>
+      </ButtonWrapper>
+    </Link>
+  )
+}
+
+export default NewAccount
