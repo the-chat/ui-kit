6d2cfbd9ee5acde6000edcdd756d8545ba25173d
the-chat <thechatllc@gmail.com>
2022-03-07 18:27:32 +0500

    contexts

--- src/utils/contexts.ts
+++ src/utils/contexts.ts
@@ -1,16 +1,47 @@
-import genContext, { SimpleProviderWrapper } from "@the-chat/gen-context"
-import { BaseUserData, State } from "@the-chat/types"
-import { AllUserData } from "@the-chat/use-user"
+import genContext from "@the-chat/gen-context"
+import { State } from "@the-chat/types"
+import usePkgSSO, { getSSOLink } from "@the-chat/use-sso"
+import { useSignOut } from "@the-chat/use-user"
+import { SSO } from "@the-chat/config"
+import { SidebarButtonConfig } from "./useSidebarButtons"
+import { Auth } from "@firebase/auth"
+import { INFO } from "@the-chat/config"
+// todo: old-comment: use-user: type default value
 
-const [useConfig, ConfigProvider] = genContext<{
+// todo???????: to types pkg???
+type ReWrite<O, P extends keyof O, NP extends string | number | symbol> = Omit<
+  O,
+  P
+> &
+  Record<NP, O[P]>
+
+export type Value = {
   sidebarOpen: State<boolean>
-  // todo: use-user: type default value
-  user: AllUserData<BaseUserData>
-}>(SimpleProviderWrapper, {
-  sidebarOpen: [false, () => {}],
-  user: null,
-})
+  signOutArgs: Parameters<typeof useSignOut>
+  noUser: boolean
+  auth: Auth
+  InfoConfig: ReWrite<typeof INFO, "DEFAULT_INFO_HOST", "HOST">
+  SSOConfig: ReWrite<typeof SSO, "DEFAULT_SSO_HOST", "HOST">
+  useSidebarButtons: () => SidebarButtonConfig[]
+}
+
+export const [useConfig, ConfigProvider] = genContext<Value>()
 
 export const useSidebarOpen = () => useConfig().sidebarOpen
-export const useUser = () => useConfig().user
-export { ConfigProvider }
+export const useSidebarButtons = () => useConfig().useSidebarButtons()
+
+export const useSignOutArgs = () => useConfig().signOutArgs
+
+export const useSSO = () => {
+  const { auth, SSOConfig, noUser } = useConfig()
+
+  return usePkgSSO(auth, SSOConfig.HOST, noUser)
+}
+export const useGetSSOLink = () => {
+  const { HOST } = useConfig().SSOConfig
+
+  return getSSOLink(HOST)
+}
+export const useSSOConfig = () => useConfig().SSOConfig
+
+export const useInfoConfig = () => useConfig().InfoConfig
