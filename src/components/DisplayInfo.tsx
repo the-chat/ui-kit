9b1bd352d81f335e5c53ed25a1d9a2ec5aac0b79
the-chat <thechatllc@gmail.com>
2022-03-11 16:43:30 +0500

    linted types

--- src/components/DisplayInfo.tsx
+++ src/components/DisplayInfo.tsx
@@ -2,15 +2,14 @@ import React from "react"
 import { Namespace, useTranslation } from "react-i18next"
 import getMarkdown, { Args, Options } from "./Markdown"
 
+type DisplayInfoProps = Partial<Options> & { tKey: Namespace }
+
 const getDisplayInfo: (
   ...args: Args
-) => (props: Partial<Options> & { tKey: Namespace }) => JSX.Element = (
-  ReactMarkdown,
-  remarkGfm
-) => {
+) => (props: DisplayInfoProps) => JSX.Element = (ReactMarkdown, remarkGfm) => {
   const Markdown = getMarkdown(ReactMarkdown, remarkGfm)
 
-  const DisplayInfo = ({ tKey, ...props }) => {
+  const DisplayInfo = ({ tKey, ...props }: DisplayInfoProps) => {
     const { t } = useTranslation("info")
 
     return <Markdown {...props}>{t(tKey)}</Markdown>
