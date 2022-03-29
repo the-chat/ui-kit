fab39e9c635922fe9e3b502c5c0803b9382bfce0
the-chat <thechatllc@gmail.com>
2022-03-29 18:00:28 +0500

    no rewriting of config pkg, markdown component in config

--- src/utils/contexts.ts
+++ src/utils/contexts.ts
@@ -2,18 +2,11 @@ import genContext from "@the-chat/gen-context"
 import { State } from "@the-chat/types"
 import usePkgSSO, { getSSOLink } from "@the-chat/use-sso"
 import { useSignOut } from "@the-chat/use-user"
-import { SSO, INFO } from "@the-chat/config"
 import { SidebarButtonConfig } from "./useSidebarButtons"
 import { Auth } from "@firebase/auth"
 import { ContainerProps } from "@mui/material"
-// todo: old-comment: use-user: type default value
-
-// todo???????: to types pkg???
-type ReWrite<O, P extends keyof O, NP extends string | number | symbol> = Omit<
-  O,
-  P
-> &
-  Record<NP, O[P]>
+import ReactMarkdown from "react-markdown"
+import { SSO } from "@the-chat/config"
 
 export type Value = {
   sidebarOpen: State<boolean>
@@ -21,9 +14,8 @@ export type Value = {
   newUser: boolean
   containerMaxWidth: ContainerProps["maxWidth"]
   auth: Auth
-  InfoConfig: ReWrite<typeof INFO, "DEFAULT_INFO_HOST", "HOST">
-  SSOConfig: ReWrite<typeof SSO, "DEFAULT_SSO_HOST", "HOST">
   useSidebarButtons: () => SidebarButtonConfig[]
+  ReactMarkdown: typeof ReactMarkdown
 }
 
 export const [useConfig, ConfigProvider] = genContext<Value>()
@@ -31,12 +23,10 @@ export const [useConfig, ConfigProvider] = genContext<Value>()
 export const useSidebarButtons = () => useConfig().useSidebarButtons()
 
 export const useSSO = () => {
-  const { auth, SSOConfig, newUser } = useConfig()
+  const { auth, newUser } = useConfig()
 
-  usePkgSSO(auth, SSOConfig.HOST, newUser)
+  usePkgSSO(auth, SSO.HOST, newUser)
 }
 export const useGetSSOLink = (path = "") => {
-  const { HOST } = useConfig().SSOConfig
-
-  return getSSOLink(HOST + path)
+  return getSSOLink(SSO.HOST + path)
 }
