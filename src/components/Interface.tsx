910ecb97baff899cdc4445aa99a710eb785e1278
the-chat <thechatllc@gmail.com>
2022-03-29 16:36:10 +0500

    Interface props types

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -2,8 +2,7 @@ import React, { FC } from "react"
 import { Container } from "@mui/material"
 import { useConfig } from "../utils/contexts"
 import Header from "../components/Header"
-import { ProvidersProps } from "../components/Providers"
-import Wrapper from "../components/Wrapper"
+import Wrapper, { WrapperProps } from "../components/Wrapper"
 
 const PageLayout: FC = ({ children }) => (
   <>
@@ -12,7 +11,7 @@ const PageLayout: FC = ({ children }) => (
   </>
 )
 
-const Interface: FC<ProvidersProps> = ({ children, ...props }) => (
+const Interface: FC<WrapperProps> = ({ children, ...props }) => (
   <Wrapper {...props}>
     <PageLayout>{children}</PageLayout>
   </Wrapper>
