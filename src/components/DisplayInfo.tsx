c5f73f34ffd2b1ca9f649f60d912eccbc6c43a30
the-chat <thechatllc@gmail.com>
2022-03-29 19:26:20 +0500

    DisplayInfo: no additional props

--- src/components/DisplayInfo.tsx
+++ src/components/DisplayInfo.tsx
@@ -1,15 +1,12 @@
 import React from "react"
 import { TFuncKey, useTranslation } from "react-i18next"
 import Markdown from "./Markdown"
-import { Options } from "react-markdown"
 
-type DisplayInfoProps = Partial<Omit<Options, "children">> & { tKey: TFuncKey }
-
-const DisplayInfo = ({ tKey, ...props }: DisplayInfoProps) => {
-  // todo?: namespace to params
+const DisplayInfo = ({ tKey }: { tKey: TFuncKey }) => {
+  // todo???: namespace to params
   const { t } = useTranslation("info")
 
-  return <Markdown {...props}>{t(tKey)}</Markdown>
+  return <Markdown>{t(tKey)}</Markdown>
 }
 
 export default DisplayInfo
