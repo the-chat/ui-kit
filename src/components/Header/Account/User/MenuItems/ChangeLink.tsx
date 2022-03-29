3feea82b7977888b89e7dc931518eb2e978c8106
the-chat <thechatllc@gmail.com>
2022-03-29 12:10:06 +0500

    user links return url

--- src/components/Header/Account/User/MenuItems/ChangeLink.tsx
+++ src/components/Header/Account/User/MenuItems/ChangeLink.tsx
@@ -1,13 +1,17 @@
 import React from "react"
 import { useTranslation } from "next-i18next"
 import LinkCover from "./LinkCover"
-import { useConfig } from "../../../../../utils/contexts"
+import { useConfig, useGetSSOLink } from "../../../../../utils/contexts"
 
 const ChangeLink = () => {
   const { t } = useTranslation("ui-kit-header")
   const { HOST, changeUrl } = useConfig().SSOConfig
 
-  return <LinkCover href={HOST + changeUrl}>{t("change-acc")}</LinkCover>
+  return (
+    <LinkCover href={useGetSSOLink(HOST) + changeUrl}>
+      {t("change-acc")}
+    </LinkCover>
+  )
 }
 
 export default ChangeLink
--- src/components/Header/Account/User/MenuItems/EditLink.tsx
+++ src/components/Header/Account/User/MenuItems/EditLink.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { useTranslation } from "next-i18next"
-import { useConfig } from "../../../../../utils/contexts"
+import { useConfig, useGetSSOLink } from "../../../../../utils/contexts"
 import LinkCover from "./LinkCover"
 
 const EditLink = () => {
   const { t } = useTranslation("ui-kit-header")
   const { HOST, editUrl } = useConfig().SSOConfig
 
-  return <LinkCover href={HOST + editUrl}>{t("edit-acc")}</LinkCover>
+  return (
+    <LinkCover href={useGetSSOLink(HOST) + editUrl}>{t("edit-acc")}</LinkCover>
+  )
 }
 
 export default EditLink
