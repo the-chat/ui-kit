c7d8ec73b6d94b8e50a6d8eb01964173ae841e39
the-chat <thechatllc@gmail.com>
2022-03-29 12:12:15 +0500

    StyledEngineProvider in Wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,19 +1,22 @@
 import React, { FC } from "react"
-import { CssBaseline } from "@mui/material"
+import { CssBaseline, StyledEngineProvider } from "@mui/material"
 import { useSSO } from "../utils/contexts"
 import Providers, { ProvidersProps } from "./Providers"
 
 const Logic = () => {
   useSSO()
+
   return null
 }
 
 const Wrapper: FC<ProvidersProps> = ({ children, ...providersProps }) => (
-  <Providers {...providersProps}>
-    <CssBaseline />
-    <Logic />
-    {children}
-  </Providers>
+  <StyledEngineProvider injectFirst>
+    <Providers {...providersProps}>
+      <CssBaseline />
+      <Logic />
+      {children}
+    </Providers>
+  </StyledEngineProvider>
 )
 
 export default Wrapper
