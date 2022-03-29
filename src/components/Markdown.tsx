0d0eec002005edb966e502c6d63cb4bd8239941f
the-chat <thechatllc@gmail.com>
2022-03-29 17:57:30 +0500

    Markdown: using of react-markdown types, MarkdownHeading using SubTitle, p and a in components commented (todo), no Args export, no "get" fn, todo

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,111 +1,96 @@
-import React, { FC } from "react"
-import Typography, { TypographyProps } from "@mui/material/Typography"
-import Link from "./Link"
-import Title from "./Title"
-import Table from "@mui/material/Table"
-import TableContainer from "@mui/material/TableContainer"
-import Paper from "@mui/material/Paper"
-import { ListItem, ListItemText, List } from "@mui/material"
+// TODO?: components props
 
-type H = FC<{ level: number }>
+import React, { ComponentProps } from "react"
+// import Link from "./Link"
+import Title, { SubTitle } from "./Title"
+import {
+  Typography,
+  TypographyProps,
+  Paper,
+  Table,
+  TableContainer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material"
+import { useConfig } from "../utils/contexts"
+import ReactMarkdown, { Components } from "react-markdown"
 
-type Components = Record<"li" | "table", FC> &
-  Record<"p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6", H> &
-  Record<"ul", (props: Record<string, unknown>) => JSX.Element> &
-  Record<"a", typeof Link>
+const MarkdownHeading: Components["h1"] = ({ level, children }) => {
+  let variant: Required<TypographyProps>["variant"]
 
-export type Options = {
-  remarkPlugins: unknown[]
-  components: Components
-  children: unknown
+  switch (level) {
+    case 1:
+      return <Title text={children} />
+    case 2:
+      return <SubTitle text={children} />
+    case 3:
+      variant = "subtitle1"
+      break
+    case 4:
+      variant = "subtitle2"
+      break
+    case 5:
+      variant = "body1"
+      break
+    case 6:
+    default:
+      variant = "body2"
+      break
+  }
+
+  return (
+    <Typography gutterBottom variant={variant}>
+      {children}
+    </Typography>
+  )
 }
 
-const getMarkdown = (
-  // todo?: types
-  ReactMarkdown: (props: Options) => JSX.Element,
-  remarkGfm: unknown
-) => {
-  const MarkdownHeading: H = ({ level, children }) => {
-    let variant: Required<TypographyProps>["variant"]
-    switch (level) {
-      case 1:
-        return <Title text={children} />
-      case 2:
-        return (
-          <Typography color="primary" gutterBottom variant="caption">
-            {children}
-          </Typography>
-        )
-      case 3:
-        variant = "subtitle1"
-        break
-      case 4:
-        variant = "subtitle2"
-        break
-      case 5:
-        variant = "body1"
-        break
-      case 6:
-      default:
-        variant = "body2"
-        break
-    }
+const MarkdownListItem: Components["li"] = ({ children }) => (
+  <ListItem divider dense>
+    <ListItemText primary={children} />
+  </ListItem>
+)
 
-    return (
-      <Typography gutterBottom variant={variant}>
-        {children}
-      </Typography>
-    )
-  }
+const MarkdownList: Components["ul"] = (props) => (
+  <List sx={{ padding: 0 }} {...props} />
+)
 
-  const MarkdownListItem: Components["li"] = ({ children }) => (
-    <ListItem divider dense>
-      <ListItemText primary={children} />
-    </ListItem>
-  )
+const MarkdownTable: Components["table"] = ({ children }) => (
+  <TableContainer component={Paper}>
+    {/* todo */}
+    <Table size="small" aria-label="a dense table">
+      {children}
+    </Table>
+  </TableContainer>
+)
 
-  const MarkdownList: Components["ul"] = (props) => (
-    <List sx={{ padding: 0 }} {...props} />
-  )
+const components: Components = {
+  h1: MarkdownHeading,
+  h2: MarkdownHeading,
+  h3: MarkdownHeading,
+  h4: MarkdownHeading,
+  h5: MarkdownHeading,
+  h6: MarkdownHeading,
+  // p: MarkdownHeading,
+  ul: MarkdownList,
+  // a: Link, // TODO
+  li: MarkdownListItem,
+  table: MarkdownTable,
+}
 
-  const MarkdownTable: Components["table"] = ({ children }) => (
-    <TableContainer component={Paper}>
-      {/* todo */}
-      <Table size="small" aria-label="a dense table">
-        {children}
-      </Table>
-    </TableContainer>
-  )
+const Markdown = (props: ComponentProps<typeof ReactMarkdown>) => {
+  const { ReactMarkdown } = useConfig().markdown
 
-  const components: Components = {
-    h1: MarkdownHeading,
-    h2: MarkdownHeading,
-    h3: MarkdownHeading,
-    h4: MarkdownHeading,
-    h5: MarkdownHeading,
-    h6: MarkdownHeading,
-    p: MarkdownHeading,
-    ul: MarkdownList,
-    a: Link,
-    li: MarkdownListItem,
-    table: MarkdownTable,
-  }
-
-  const Markdown = (props: Partial<Options>) => (
+  return (
     <ReactMarkdown
       {...props}
       components={Object.assign(components, props.components)}
-      remarkPlugins={[
-        remarkGfm,
-        ...(props.remarkPlugins ? props.remarkPlugins : []),
-      ]}
+      remarkPlugins={props.remarkPlugins}
     >
       {props.children || ""}
     </ReactMarkdown>
   )
-
-  return Markdown
 }
 
-export type Args = Parameters<typeof getMarkdown>
-export default getMarkdown
+export default Markdown
