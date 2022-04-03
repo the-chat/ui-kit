6c1d5a58b02b08ee88f4ab169d1c0e58d30f6af8
the-chat <thechatllc@gmail.com>
2022-04-03 18:16:02 +0500

    exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,39 @@
+// DisplayInfo
+export { default as DisplayInfo } from "./DisplayInfo"
+
 // DocumentHead
 export * from "./DocumentHead"
 export { default as DocumentHead } from "./DocumentHead"
-
-// DisplayInfo
-export { default as DisplayInfo } from "./DisplayInfo"
 
 // Interface
 export { default as Interface } from "./Interface"
 export { WrapperProps } from "./Interface/Wrapper"
 
+// ErrorMessage
+export { default as ErrorMessage } from "./ErrorMessage"
+
+// Flex
+export { default as Flex } from "./Flex"
+
+// InputFile
+export { default as InputFile } from "./InputFile"
+
 // Link
 export * from "./Link"
 export { default as Link } from "./Link"
+
+// Loading
+export { default as Loading } from "./Loading"
+
+// MenuCover
+export { default as MenuCover } from "./MenuCover"
+
+// PasswordTextField
+export { default as PasswordTextField } from "./PasswordTextField"
+
+// SignedInOnly
+export * from "./SignedInOnly"
+export { default as SignedInOnly } from "./SignedInOnly"
 
 // Title
 export * from "./Title"
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./contexts"
 export * from "./useSidebarButtons"
+export { default as flex } from "./flex"
