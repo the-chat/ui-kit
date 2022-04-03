4704b9b2954f2cf72a41bae343eea2b22ef1140f
the-chat <thechatllc@gmail.com>
2022-04-03 17:58:27 +0500

    InputFile

---
+++ src/components/InputFile.tsx
@@ -0,0 +1,55 @@
+import { makeStyles } from "@mui/styles"
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate"
+import { TextField, TextFieldProps } from "@mui/material"
+import { NoChildrenComponent } from "@the-chat/types"
+
+const useStyles = makeStyles({
+  hiddenFileInput: {
+    position: "absolute",
+    width: 1,
+    height: 1,
+    padding: 0,
+    overflow: "hidden",
+    clip: "rect(0, 0, 0, 0)",
+    whiteSpace: "nowrap",
+    clipPath: "inset(50%)",
+    border: 0,
+  },
+})
+
+type DT = {
+  component: string
+  disabled: boolean
+  children: Element
+}
+
+const InputFile = <T extends DT>({
+  Tag,
+  children = <AddPhotoAlternateIcon />,
+  disabled = false,
+  componentProps,
+  ...props
+}: {
+  Tag: NoChildrenComponent<T>
+  componentProps: Omit<T, keyof DT>
+} & TextFieldProps) => {
+  const { hiddenFileInput } = useStyles()
+
+  return (
+    <Tag {...(componentProps as T)} component="label" disabled={disabled}>
+      {/* learn: in html "<input type="file" ... value>" should i fix than and is this is a bug? */}
+      <>
+        <TextField
+          // props not overriding component behavior
+          {...props}
+          disabled={disabled}
+          className={[props.className, hiddenFileInput].join(" ")}
+          type="file"
+        />
+        {children}
+      </>
+    </Tag>
+  )
+}
+
+export default InputFile
