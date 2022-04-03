acc89f0625b2aacb678abbb0bac4920083bd036b
the-chat <thechatllc@gmail.com>
2022-04-03 17:57:44 +0500

    ErrorMessage

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material"
+import { useTranslation } from "next-i18next"
+
+export type Log = (msg: unknown) => void
+
+const ErrorMessage = ({
+  log = console.dir,
+  error,
+}: {
+  log: Log
+  error: Error
+}) => {
+  const { t } = useTranslation("fallbacks")
+
+  log(error)
+
+  return (
+    <Typography variant="body1">
+      {t("error", {
+        errorMessage: error,
+      })}
+    </Typography>
+  )
+}
+
+export default ErrorMessage
