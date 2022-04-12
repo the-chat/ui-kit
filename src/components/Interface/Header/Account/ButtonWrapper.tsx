3e83b586e94017aacc3d1c1425afaef7179c07d0
the-chat <thechatllc@gmail.com>
2022-04-12 23:58:52 +0500

    ButtonWrapper ref

--- src/components/Interface/Header/Account/ButtonWrapper.tsx
+++ src/components/Interface/Header/Account/ButtonWrapper.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { forwardRef, PropsWithChildren } from "react"
 import { IconButton, IconButtonProps, Theme } from "@mui/material"
 import { makeStyles } from "@mui/styles"
 import TooltipWithOnClick from "../../../TooltipWithOnClick"
@@ -8,32 +8,37 @@ const useStyles = makeStyles<Theme>((theme) => ({
   open: headerButtonStyles(theme, true),
 }))
 
-const ButtonWrapper: FC<
+type ButtonWrapperProps = PropsWithChildren<
   Pick<IconButtonProps, "onClick"> & {
     title: string
   }
-> = ({ children, onClick, title }) => {
-  const { open } = useStyles()
+>
 
-  return (
-    <TooltipWithOnClick
-      title={title}
-      tooltipProps={{
-        className: open,
-      }}
-      ComponentTag={IconButton}
-      componentProps={{
-        style: {
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "centet",
-        },
-        ...(onClick && { onClick }),
-      }}
-    >
-      {children}
-    </TooltipWithOnClick>
-  )
-}
+const ButtonWrapper = forwardRef<unknown, ButtonWrapperProps>(
+  ({ children, onClick, title }, ref) => {
+    const { open } = useStyles()
+
+    return (
+      <TooltipWithOnClick
+        title={title}
+        tooltipProps={{
+          className: open,
+        }}
+        ref={ref}
+        ComponentTag={IconButton}
+        componentProps={{
+          style: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "centet",
+          },
+          ...(onClick && { onClick }),
+        }}
+      >
+        {children}
+      </TooltipWithOnClick>
+    )
+  }
+)
 
 export default ButtonWrapper
